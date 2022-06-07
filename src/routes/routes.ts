c63999d865844ccfa338d0ecf85c3ca159090aeb
Juan Silupu <juansilupumaza@gmail.com>
2022-06-06 22:15:10 -0500

    creacion de un modulo que carga de manera peresoza (Lazy)

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { lazy, LazyExoticComponent } from "react"
+import { NoLazy } from "../01-lazyload/pages/NoLazy"
 
 type JSXComponent = () => JSX.Element
 interface IRoute{
@@ -8,24 +9,15 @@ interface IRoute{
     children?: IRoute[]
 }
 
-const LazyPage1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */'../01-lazyload/pages/LazyPage1'))
-const LazyPage2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */'../01-lazyload/pages/LazyPage2'))
-const LazyPage3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */'../01-lazyload/pages/LazyPage3'))
-
 export const routes: IRoute[] = [
     {
-        path:"/lazy1",
-        Component: LazyPage1,
-        name: 'Lazy-Page1'
+        path:"/lazyload",
+        Component: lazy(() => import(/* webpackChunkName: "LazyLayout" */'../01-lazyload/layout/LazyLayout')),
+        name: 'LazyLoading Nested'
     },
     {
-        path:"/lazy2",
-        Component: LazyPage2,
-        name: 'Lazy-Page2'
-    },
-    {
-        path:"/lazy3",
-        Component: LazyPage3,
-        name: 'Lazy-Page3'
-    },
+        path:'/no-lazy',
+        Component: NoLazy,
+        name: 'No Lazy'
+    }
 ]
